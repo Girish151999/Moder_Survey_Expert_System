bd92941d2940c6eddb1ef4a660303bd4e1407321
suyogsalpure0809 <suyogsalpure0809@gmail.com>
2021-07-08 11:34:19 +0530

    Entered new class

---
+++ ModernSurveyExpertSystem/src/main/java/com/SurveyExpertSystem/ModernSurveyExpertSystem/exception/xyzexception.java
@@ -0,0 +1,8 @@
+package com.SurveyExpertSystem.ModernSurveyExpertSystem.exception;
+
+public class xyzexception extends RuntimeException{
+	public void UserNotFound(String message) {
+//		super(message);
+	}
+}
+
